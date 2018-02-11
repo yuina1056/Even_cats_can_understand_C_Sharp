cada967044997821a645d2a802d8aeb669861650
Natsuki Wada <yuina.i.1056@gmail.com>
2018-02-11 17:30:34 +0900

    Practice Finish

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -7,6 +7,12 @@ namespace Practice
         public static void Main(string[] args)
         {
             Console.WriteLine("猫でも分かるC#プログラミング：練習問題");
+
+            Console.WriteLine("下記に文字列を入力してください");
+            string inputstr = Console.ReadLine();
+
+            Console.WriteLine(inputstr);
+
         }
     }
 }
